29e93feaeb489e7cdbaaaad2663a9e6d46c7d42f
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-04-07 11:56:09 -0500

    Update engine.py

--- sandsea/engine.py
+++ sandsea/engine.py
@@ -8,20 +8,22 @@ import gui
 # # Length: m
 # # Energy: J
 
+# gravity=objects.Vector(0, 0, 0), airDensity=0, precisionFactor=1e-16, gravitationEnabled=True, tickRate=1000
+
 class PropertiesController:
-	def __init__(self, gravity=objects.Vector(0, 0, 0), airDensity=0, precisionFactor=1e-16, gravitationEnabled=True, tickRate=1000):
+	def __init__(self, **properties):
 		self.properties = {
-			"gravity": gravity,
-			"airDensity": airDensity,
-			"precisionFactor": precisionFactor,
-			"gravitationEnabled": gravitationEnabled,
-			"tickRate": tickRate
+			"gravity": properties["gravity"],
+			"airDensity": properties["airDensity"],
+			"precisionFactor": properties["precisionFactor"],
+			"gravitationEnabled": properties["gravitationEnabled"],
+			"tickRate": properties["tickRate"]
 		}
-		self.gravity = gravity
-		self.airDensity = airDensity
-		self.precisionFactor = precisionFactor
-		self.gravitationEnabled = gravitationEnabled
-		self.tickRate = tickRate
+		self.gravity = properties["gravity"]
+		self.airDensity = properties["airDensity"]
+		self.precisionFactor = properties["precisionFactor"]
+		self.gravitationEnabled = properties["gravitationEnabled"]
+		self.tickRate = properties["tickRate"]
 
 	def setGravity(self, newValue):
 		self.gravity = newValue
@@ -62,6 +64,58 @@ class PropertiesController:
 			self.setGravitationEnabled(value)
 		if name == "tickRate":
 			self.setTickRate(value)
+			
+class Material:
+	def __init__(self, density=1, electricalResistance=0, magnetic=True, reflectance=1, meltingPoint=1000, boilingPoint=2000):
+		self.density = density
+		self.electricalResistance = electricalResistance
+		self.magnetic = magnetic
+		self.reflectance = reflectance
+		self.meltingPoint = meltingPoint
+		self.boilingPoint = boilingPoint
+		
+	def stateOfMatter(self, temperatureK):
+		if temperatureK <= meltingPoint:
+			return 0
+		elif temperatureK <= boilingPoint:
+			return 1
+		else:
+			return 2
+		
+class Asset:
+	def __init__(self, materialObject, *objects):
+		self.objects = objects
+		self.materialProperties = materialObject
+		self.volume = sum([obj.volume for obj in self.objects if obj.isReal])
+		self.mass = self.material.density * self.volume
+		self.movement = objects.Vector(0, 0, 0)
+		self.momentum = self.movement.length * self.mass
+		self.centerOfMass = objects.Point(0, 0, 0)
+		[self.centerOfMass.translate(obj.centerOfMass.x / len(self.objects), 0, 0) for obj in self.objects]
+		[self.centerOfMass.translate(0, obj.centerOfMass.y / len(self.objects), 0) for obj in self.objects]
+		[self.centerOfMass.translate(0, 0, obj.centerOfMass.z / len(self.objects)) for obj in self.objects]
+		self.parentSimulation = None
+
+	def __iter__(self):
+		self.n = 0
+		return self
+	
+	def __next__(self):
+		if self.n < len(self.objects):
+			out = self.objects[self.n]
+			self.n += 1
+		else:
+			raise StopIteration
+		return out
+	
+	def setParentSimulation(self, parentSimulation):
+		try:
+			self.parentSimulation = parentSimulation
+			return True
+		except NameError:
+			return False
+		except Exception as e:
+			return e
 
 class Simulation:
 	help = """HELP - Simulation
@@ -91,13 +145,25 @@ METHODS
 					     from the objects in the simulation.
 """
 
-	def __init__(self, *objects, **properties):
+	def __init__(self, *assets, **properties):
 		self.simulationTime = 0
-		self.objects = objects
+		self.assets = assets
+		self.objects = []
+		[self.objects += asset.objects for asset in self.assets]
 		self.points = []
-		[self.points.append(x) for x in [obj.__iter__() for obj in self.objects]]
-		self.centerOfMass = objects.Point(sum([p.x for p in self.points]) / len(self.points), sum([p.y for p in self.points]) / len(self.points), sum([p.z for p in self.points]) / len(self.points))
-
+		[self.points += [[x for x in iter(obj)] for obj in asset] for asset in self.assets]
+		self.centroid = objects.Point(
+			sum([p.x for p in self.points]) / len(self.points),
+			sum([p.y for p in self.points]) / len(self.points),
+			sum([p.z for p in self.points]) / len(self.points)
+		)
+		self.centerOfMass = objects.Point(
+			sum([asset.centerOfMass.x for asset in self.assets]) / len(self.assets),
+			sum([asset.centerOfMass.y for asset in self.assets]) / len(self.assets),
+			sum([asset.centerOfMass.z for asset in self.assets]) / len(self.assets)
+		)
+		self.properties = PropertiesController(**properties)
+		
 	def __iter__(self):
 		self.n = 0
 		return self
@@ -151,9 +217,7 @@ METHODS
 			return False
 
 	def add(self, *objects):
-		for x in objects:
-			self.objects.append(x)
+		self.__init__(*self.objects + objects, **self.properties.properties)
 
 	def remove(self, *indices):
-		for x in indices:
-			del self.objects[x]
+		self.__init__(*[self.objects[x] for x in range(len(self.objects)) if not x in indices], **self.properties.properties)
