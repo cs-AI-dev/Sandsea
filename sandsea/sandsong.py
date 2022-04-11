1d23d5a34bfbfa58dbb92161659c0ba11890d648
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-04-11 11:57:07 -0500

    Update sandsong.py

--- sandsea/sandsong.py
+++ sandsea/sandsong.py
@@ -2,9 +2,14 @@ import objects
 import engine
 import gui
 
-def interpretText(text):
-	gui.
+def interpretText(text, parsingSandsongInitialTextSend=True):
+	if parsingSandsongInitialTextSend: gui.log("Parsing Sandsong ...")
+	
+	
 
 def interpretFile(filedir):
+	gui.log("Loading file to parse ...")
 	with open(filedir, "r") as f:
-		interpretText(f.read())
+		gui.log("    ┣━ done."
+		gui.log("    ┗━ Beginning parse ...")
+		interpretText(f.read(), parsingSandsongInitialTextSend=False)
