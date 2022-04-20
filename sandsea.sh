8f1e2af32569c5e879bbf4391c6563733918d685
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-04-20 11:18:48 -0500

    Update sandsea.sh

--- sandsea.sh
+++ sandsea.sh
@@ -1,8 +1,8 @@
 type systemdata/version.dat
-echo Installing PIP upgrade
-py -m pip install --upgrade pip -q
-echo done.
-echo Installing required libs
+echo Installing PIP upgrade.
+py -m pip install --upgrade pip
+cls
+echo Installing required libs.
 py -m pip install -r systemdata/requirements.dat -q --no-warn-script-location
-echo done.
+cls
 py sandsea/sandsea.py loadgui
